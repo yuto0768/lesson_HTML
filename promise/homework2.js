7451cc295f1d7dfac5d014b53ade7619fc9da8b9
yuto sakai <yuto0768@gmail.com>
2020-01-19 09:21:00 +0900

    homework2

--- promise/homework2.js
+++ promise/homework2.js
@@ -1,17 +1,24 @@
 const fs = require("fs");
 
-//宿題2
 //loadFile()は、エラーが発生した際に、throwされ、実行が中断されます。
 //エラーが発生した時に、実行が中断されず、戻り地がnullになるようにしてください
-function loadFile(file, callback) {
-  let data = fs.readFileSync(file);
-  return data;
-}
-
 //ファイルを読み込んで、内容を返します
+function loadFile(file, callback) {
+  try{
+    let data = fs.readFileSync(file, {encoding:"UTF-8"});
+    return data;
+
+  }catch(error){
+    return null;
+  }  
+  
+}
+  
+
+
 var data = loadFile("./hello.txt");
 console.log(data);
 
-//ファイルを読み込んで、内容を返します
+
 var nashi = loadFile("./nachi.txt");
 console.log(nashi);
